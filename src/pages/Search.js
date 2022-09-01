b576a7aaa67f7c66d8bb4218cca67f2da48ebd2f
moisesfdasilva <msesfernandes@gmail.com>
2022-09-01 11:41:15 -0300

    Realizado o requisito 6, em Search.js.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
@@ -6,11 +7,14 @@ class Search extends React.Component {
   state = {
     isSearchButtonDisabled: true,
     searchValue: '',
+    searchAlbuns: '',
+    loading: false,
+    artist: '',
   };
 
-  componentDidMount() {
-    // aaaaa
-  }
+  // componentWillUnmount() {
+  //   this.setState({ usedButtonSearch: false });
+  // }
 
   searchTextInput = (action) => {
     const searchText = action.target.value;
@@ -28,23 +32,31 @@ class Search extends React.Component {
     }
   };
 
-  searchButton = () => {
+  searchButton = async () => {
     const { searchValue } = this.state;
-    const searchArist = searchValue;
+    this.setState({ loading: true });
+    const searchData = await searchAlbumsAPI(searchValue);
     this.setState({
       isSearchButtonDisabled: false,
+      artist: searchValue,
       searchValue: '',
+      searchAlbuns: searchData,
+      loading: false,
+    }, () => {
+      const { searchAlbuns } = this.state;
+      console.log(searchAlbuns);
     });
-    searchAlbumsAPI(searchArist);
   };
 
   render() {
-    const { isSearchButtonDisabled } = this.state;
+    const { isSearchButtonDisabled, searchValue, searchAlbuns,
+      loading, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <form>
           <input
+            value={ searchValue }
             type="text"
             data-testid="search-artist-input"
             onChange={ this.searchTextInput }
@@ -58,6 +70,30 @@ class Search extends React.Component {
             Pesquisar
           </button>
         </form>
+        <article>
+          <div>
+            { loading ? <h3>Carregando...</h3> : '' }
+            { (typeof searchAlbuns === 'object') ? (
+              <h2>{ `Resultado de álbuns de: ${artist} `}</h2>) : '' }
+          </div>
+          <section>
+            { (typeof searchAlbuns === 'object') && (searchAlbuns.length > 0) && (
+              searchAlbuns.map((album, index) => (
+                <div key={ index }>
+                  <h4>{ album.collectionName }</h4>
+                  <p>{ album.artistName }</p>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                  </Link>
+                </div>
+              ))) }
+            { (typeof searchAlbuns === 'object') && (searchAlbuns.length === 0) && (
+              <h2>Nenhum álbum foi encontrado</h2>) }
+          </section>
+        </article>
       </div>
     );
   }
