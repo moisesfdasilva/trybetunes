ec7cf366e7531a9290a3be03d32be0911986aa10
moisesfdasilva <msesfernandes@gmail.com>
2022-08-31 21:40:23 -0300

    Realizado o requisito 3, em Header.js.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
+  state = {
+    loading: true,
+    user: '',
+  };
+
+  async componentDidMount() {
+    const userObject = await getUser();
+    const userName = userObject.name;
+    this.setState({
+      loading: false,
+      user: userName,
+    });
+  }
+
   render() {
+    const { loading, user } = this.state;
     return (
       <header data-testid="header-component">
         <Link to="/search" data-testid="link-to-search">Search</Link>
         <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
         <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        <h3 data-testid="header-user-name">{ user }</h3>
+        { loading ? <h3>Carregando...</h3> : '' }
       </header>
     );
   }
